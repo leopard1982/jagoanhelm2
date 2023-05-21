9aa4dc42e76209c8c06273cf2b2d1a6d9dae35bd
leopard1982 <suryoadhychandra@outlook.com>
2023-05-21 11:49:12 +0700

    adding static and media root in urls.py

--- jagoanhelm/urls.py
+++ jagoanhelm/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('penjualan.urls'))
+    path('',include('penjualan.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, kwargs={'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, kwargs={'document_root': settings.STATICFILES_DIRS}),
+
 ]
