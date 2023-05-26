518325167d41d7f6bf41472d4912ebab739fdf71
leopard1982 <suryoadhychandra@outlook.com>
2023-05-26 15:02:46 +0700

    upload/download kategori ok, file hasil upload langsung hapus

--- administrasi/reads.py
+++ administrasi/reads.py
@@ -38,6 +38,7 @@ class readdata:
 				
 			except:
 				pass
+		os.remove(pathnya)
 		return jumlah_data
 
 	def insertToTableKategori(pathnya):
@@ -63,6 +64,8 @@ class readdata:
 				jumlah_data+=1
 			except:
 				pass
+
+		os.remove(pathnya)
 		# for xx in x:
 		# 	print(xx)
 		# 	try:
